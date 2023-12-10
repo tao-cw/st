deb9b90141f868931fa3e08dd0071d7d62fc5916
taocw <taocw@foxmail.com>
2023-12-10 23:00:22 +0800

    set font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=24";
 static int borderpx = 2;
 
 /*
