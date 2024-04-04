2d64386c7986924a33d2947691ea311ece522e06
taocw <taocw@foxmail.com>
2024-04-04 08:46:52 +0800

    upstream: 497a75638291454875ba1ec8d484c7f3d6f41d66

--- x.c
+++ x.c
@@ -2023,6 +2023,9 @@ xseticontitle(char *p)
 	XTextProperty prop;
 	DEFAULT(p, opt_title);
 
+	if (p[0] == '\0')
+		p = opt_title;
+
 	if (Xutf8TextListToTextProperty(xw.dpy, &p, 1, XUTF8StringStyle,
 	                                &prop) != Success)
 		return;
@@ -2036,6 +2039,9 @@ xsettitle(char *p)
 {
 	XTextProperty prop;
 	DEFAULT(p, opt_title);
+
+	if (p[0] == '\0')
+		p = opt_title;
 
 	if (Xutf8TextListToTextProperty(xw.dpy, &p, 1, XUTF8StringStyle,
 	                                &prop) != Success)
