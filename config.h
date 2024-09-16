40283a06d58063f905338507b9efea0ea6b1b656
taocw <taocw@foxmail.com>
2024-09-17 02:07:33 +0800

    decrease borderpx

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "mono:size=14:antialias=true:autohint=true";
-static int borderpx = 10;
+static int borderpx = 5;
 
 /*
  * What program is execed by st depends of these precedence rules:
